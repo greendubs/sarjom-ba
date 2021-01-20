4696ea5c2899885a5201944303b3b8492b31c67c
Kiranmayi Klc <user.name>
2021-01-20 08:48:08 +0530

    Update copyright on footer

--- gatsby-app-starter-rest-api/src/components/theme/Footer.js
+++ gatsby-app-starter-rest-api/src/components/theme/Footer.js
@@ -6,36 +6,31 @@ import { Typography } from '@material-ui/core'
 export default ({ noPad }) => (
   <footer className={styles.footer} style={{ padding: '1rem' }}>
     <div className="center-text flex-container container">
-      <Typography variant='body2' style={{ color: 'white' }}>
-        @Sarjom, CitSci Earth Lab, Seattle, USA
+      <Typography variant="body2" style={{ color: 'white' }}>
+        @Sarjom Project, Seattle, USA
       </Typography>
       <nav>
-      <Typography variant='body1' style={{ color: 'white' }}>
-        <Link to="/help" className={styles.footernav}>
-        
-          Help and Support
-      
-        </Link>
-        {`  `}
-        <Link to="/market-place" className={styles.footernav}>
-  
-          Eco-marketplace
-  
-        </Link>
-        {`  `}
-        <Link to="/feedback" className={styles.footernav}>
-
-          Feedback
-        </Link>
-        {`  `}
-        <a
-          href="https://github.com/greendubs"
-          rel="noopener noreferrer"
-          className={styles.footernav}
-        >
-          Github
-        </a>
-        {`  `}
+        <Typography variant="body1" style={{ color: 'white' }}>
+          <Link to="/help" className={styles.footernav}>
+            Help and Support
+          </Link>
+          {`  `}
+          <Link to="/market-place" className={styles.footernav}>
+            Eco-marketplace
+          </Link>
+          {`  `}
+          <Link to="/feedback" className={styles.footernav}>
+            Feedback
+          </Link>
+          {`  `}
+          <a
+            href="https://github.com/greendubs"
+            rel="noopener noreferrer"
+            className={styles.footernav}
+          >
+            Github
+          </a>
+          {`  `}
         </Typography>
       </nav>
     </div>
